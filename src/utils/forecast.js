2e38ae6a98655576ea2137e4f2edaebc77714fb9
subhanisyed17 <subhanisyed17@gmail.com>
2020-05-25 16:52:55 -0400

    added humidity data to weather info and modified about page desc

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude,longitude,callback) => {
         }
         else{
             callback(undefined,
-                `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degrees out and It feels like ${body.current.feelslike} degrees and `,
+                `${body.current.weather_descriptions[0]}.It is currently ${body.current.temperature} degrees out and It feels like ${body.current.feelslike} degrees, humidity is ${body.current.humidity}% `
             )
         }
     })
